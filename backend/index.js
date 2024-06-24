d9423d0d152bfbcbceb6193cfdd2553e7933b95e
Madushan Sandakelum <75947205+CillianCoder@users.noreply.github.com>
2024-06-25 01:20:27 +0530

    build table template and operation links

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,12 @@
 import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
-import booksRoutes from "./routes/booksRoutes.js"; 
+import booksRoutes from "./routes/booksRoutes.js";
 import cors from "cors";
 
 const app = express();
 
-// Middleware for parsing 
+// Middleware for parsing
 app.use(express.json());
 
 //Middleware for CORS Policy
@@ -19,22 +19,22 @@ app.use(cors());
 // }));
 
 app.get("/", (req, res) => {
-    console.log(req);
-    return res.status(234).send("Welcome");
+  console.log(req);
+  return res.status(234).send("Welcome");
 });
 
 // Route middleware
-app.use('/books', booksRoutes);
+app.use("/books", booksRoutes);
 
 // Database and server
 mongoose
-    .connect(mongoDBURL)
-    .then(() => {
-        console.log("Connected to MongoDB");
-        app.listen(PORT, () => {
-            console.log(`Server is running on port ${PORT}`);
-        });
-    })
-    .catch((err) => {
-        console.log(err);
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
     });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
