5115b7ae45dada66198b7847fc6b72f6661d7694
Madushan Sandakelum <75947205+CillianCoder@users.noreply.github.com>
2024-07-14 17:52:36 +0530

    Add book model

---
+++ frontend/src/components/home/BookModel.jsx
@@ -0,0 +1,45 @@
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable react/prop-types */
+import { AiOutlineClose } from "react-icons/ai";
+import { PiBookOpenTextLight } from "react-icons/pi";
+import { BiUserCircle } from "react-icons/bi";
+
+const BookModel = ({ book, onClose }) => {
+  return (
+    <div
+      className="fixed bg-black bg-opacity-60 top-0 left-0 right-0 bottom-0 z-50 flex justify-center items-center"
+      onClick={onClose}>
+      <div
+        onClick={(event) => event.stopPropagation()}
+        className="w-[600px] max-w-full h-[400px] bg-white rounded-xl p-4 flex flex-col relative">
+        <AiOutlineClose
+          className="absolute right-6 top-6 text-3xl text-red-600 cursor-pointer"
+          onClick={onClose}
+        />
+        <h2 className="w-fit px-4 py-1 bg-red-300 rounded-lg">
+          {book.publishYear}
+        </h2>
+        <h4 className="my-2 text-gray-500">{book._id}</h4>
+        <div className="flex justify-start items-center gap-x-2">
+          <PiBookOpenTextLight className="text-red-300 text-2xl" />
+          <h2 className="my-1">{book.title}</h2>
+        </div>
+        <div className="flex justify-start items-center gap-x-2">
+          <BiUserCircle className="text-red-300 text-2xl" />
+          <h2 className="my-1">{book.author}</h2>
+        </div>
+        <p className="mt-4">Anything you want to show</p>
+        <p className="my-4">
+          A TXT file, short for "text file" is a type of computer file that
+          stores plain text information without any formatting. These files are
+          widely used for various purposes, such as storing data, writing
+          scripts, and exchanging information between different programs and
+          platforms. TXT files are simple and easy to create and edit, making
+          them popular for a wide range of applications.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default BookModel;
